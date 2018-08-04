225cc6d200295285922ff2c1328ab665d489865b
Megan <megan.lkm@gmail.com>
2018-08-04 15:09:16 -0500

    tests

--- tests/test_things.py
+++ tests/test_things.py
@@ -1,7 +1,39 @@
 from unittest import TestCase
 
+from aws_lookup import core
 
-class AWSLookupTest(TestCase):
 
-    def test_nothing(self):
-        self.assertTrue(True)
+class CoreTest(TestCase):
+
+    def test_filter_objects_rtn_list(self):
+        res = core.filter_objects(
+            [
+                {'foo': "this is a value"},
+                {'foo': "bar"},
+            ],
+            '*is*',
+            'foo'
+        )
+        self.assertListEqual(res, ["this is a value"])
+
+    def test_filter_objects_rtn_dict(self):
+        res = core.filter_objects(
+            [
+                {'foo': "this is a value"},
+                {'foo': "bar"},
+            ],
+            'bar',
+            'foo',
+            True
+        )
+        self.assertListEqual(res, [{'foo': "bar"}])
+
+    def test_filter_objects_no_key(self):
+        res = core.filter_objects(
+            [
+                "this is a value",
+                "bar",
+            ],
+            '*lue'
+        )
+        self.assertListEqual(res, ['this is a value'])
