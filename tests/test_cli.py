21a5c4c3f6487237e7226eb87fc5b644ef4b324d
Megan <megan.lkm@gmail.com>
2018-08-28 14:34:37 -0500

    cli test

---
+++ tests/test_cli.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from click.testing import CliRunner
+
+from aws_lookup import cli as aws_lookup_cli
+
+CMD_OPTS = ['--version', ' -h, ?, --help']
+
+
+class CLITest(TestCase):
+
+    def setUp(self):
+        self.runner = CliRunner()
+
+    def test_cmd_help(self):
+        for help_flag in ['--help', '?', '-h']:
+            result = self.runner.invoke(aws_lookup_cli.cli, [help_flag])
+            for opt in CMD_OPTS:
+                self.assertIn(opt, result.output)
